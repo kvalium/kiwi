6a602fa1157984c3e8303c94d3ad8cc950c81b53
Julien MONCHANY <julien.monchany@gmail.com>
2014-09-19 10:10:21 +0200

    Init

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+kvalium:$1$XiPNCoAM$9wSHo.wBqSyik4thbcVEH.:Julien:julien.monchany@gmail.com:admin,user
